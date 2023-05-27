3ceeb2d50b97ce4e1f2b37c07ae030a75ad8c2c6
Richard Beneyton <richard.beneyton@gmail.com>
2023-05-27 17:09:49 +0200

    template

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,61 @@
+use std::io;
+
+macro_rules! parse_input {
+    ($x:expr, $t:ident) => ($x.trim().parse::<$t>().unwrap())
+}
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
 fn main() {
-    println!("Hello, world!");
+    let mut input_line = String::new();
+    io::stdin().read_line(&mut input_line).unwrap();
+    let number_of_cells = parse_input!(input_line, i32); // amount of hexagonal cells in this map
+    for i in 0..number_of_cells as usize {
+        let mut input_line = String::new();
+        io::stdin().read_line(&mut input_line).unwrap();
+        let inputs = input_line.split(" ").collect::<Vec<_>>();
+        let _type = parse_input!(inputs[0], i32); // 0 for empty, 1 for eggs, 2 for crystal
+        let initial_resources = parse_input!(inputs[1], i32); // the initial amount of eggs/crystals on this cell
+        let neigh_0 = parse_input!(inputs[2], i32); // the index of the neighbouring cell for each direction
+        let neigh_1 = parse_input!(inputs[3], i32);
+        let neigh_2 = parse_input!(inputs[4], i32);
+        let neigh_3 = parse_input!(inputs[5], i32);
+        let neigh_4 = parse_input!(inputs[6], i32);
+        let neigh_5 = parse_input!(inputs[7], i32);
+    }
+    let mut input_line = String::new();
+    io::stdin().read_line(&mut input_line).unwrap();
+    let number_of_bases = parse_input!(input_line, i32);
+    let mut inputs = String::new();
+    io::stdin().read_line(&mut inputs).unwrap();
+    for i in inputs.split_whitespace() {
+        let my_base_index = parse_input!(i, i32);
+    }
+    let mut inputs = String::new();
+    io::stdin().read_line(&mut inputs).unwrap();
+    for i in inputs.split_whitespace() {
+        let opp_base_index = parse_input!(i, i32);
+    }
+
+    // game loop
+    loop {
+        for i in 0..number_of_cells as usize {
+            let mut input_line = String::new();
+            io::stdin().read_line(&mut input_line).unwrap();
+            let inputs = input_line.split(" ").collect::<Vec<_>>();
+            let resources = parse_input!(inputs[0], i32); // the current amount of eggs/crystals on this cell
+            let my_ants = parse_input!(inputs[1], i32); // the amount of your ants on this cell
+            let opp_ants = parse_input!(inputs[2], i32); // the amount of opponent ants on this cell
+        }
+
+        // Write an action using println!("message...");
+        // To debug: eprintln!("Debug message...");
+
+
+        // WAIT | LINE <sourceIdx> <targetIdx> <strength> | BEACON <cellIdx> <strength> | MESSAGE <text>
+        println!("WAIT");
+    }
+}
 }
